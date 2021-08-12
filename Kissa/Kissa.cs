1829c5134c9cd324593db64cdf5053e368c2449a
Markus <29477753+Marakusa@users.noreply.github.com>
2021-08-12 10:01:20 +0300

    Add files via upload

---
+++ Kissa/Kissa.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kissa
+{
+	public class Kissa
+	{
+		private int ika;
+		public string nimi;
+
+		public Kissa()
+		{
+			this.ika = 0;
+			this.nimi = "Miuku";
+		}
+		public Kissa(int ika, string nimi)
+		{
+			this.ika = ika;
+			this.nimi = nimi;
+		}
+
+		/// <summary>
+		/// Asettaa kissalle uuden nimen.
+		/// </summary>
+		/// <param name="nimi">Kissan uusi nimi.</param>
+		/// <returns></returns>
+		public bool AsetaKissanNimi(string nimi)
+		{
+			if (nimi.ToLower() != "hilda")
+			{
+				this.nimi = nimi;
+				return true;
+			}
+
+			return false;
+		}
+		public bool AsetaKissanIka(int ika)
+		{
+			if (ika >= 0)
+			{
+				this.ika = ika;
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
---
+++ Kissa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Kissa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kissa katti = new Kissa();
+
+            Console.WriteLine($"Kissan nimi: {katti.nimi}");
+            Console.WriteLine($"Nimen vaihto: {katti.AsetaKissanNimi("Anneli")}");
+            Console.WriteLine($"Kissan nimi: {katti.nimi}");
+            Console.WriteLine($"Nimen vaihto: {katti.AsetaKissanNimi("Hilda")}");
+            Console.WriteLine($"Kissan nimi: {katti.nimi}");
+
+            Console.ReadKey();
+        }
+    }
+}
