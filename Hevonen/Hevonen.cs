67d9175ba80adae9fd9c0281b1b92610b8d5a50e
Marakusa <29477753+Marakusa@users.noreply.github.com>
2021-08-13 10:25:32 +0300

    Added hevonen

---
+++ Hevonen/Hevonen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Hevonen
+{
+    public class Hevonen : Nisakkaat.Nisakkaat
+	{
+		public readonly string[] hevosenNimet = new string[] { "Hirnu", "Hepo", "Max", "Ella" };
+		public string[] Nimet
+		{
+			get
+			{
+				return hevosenNimet;
+			}
+		}
+
+		public Hevonen()
+		{
+			this.AsetaNimi("Haukkuli");
+		}
+		public Hevonen(int ika, string nimi)
+		{
+			this.AsetaIka(ika);
+			this.AsetaNimi(nimi);
+		}
+
+		public override void Aantele()
+		{
+			Console.WriteLine("Hevonen hirnahtaa");
+		}
+
+		// Override functions
+		public override string ToString()
+		{
+			return $"Hevonen: {this.PalautaNimi()}, ikä {this.PalautaIka()}, {(this.PalautaOnLihanSyoja() ? "Lihansyöjä" : "Ei ole lihansyöjä")}, {this.PalautaOnkoMerinisakas()}.";
+		}
+	}
+}
--- OliotListaan/Program.cs
+++ OliotListaan/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ClassLibrary;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OliotListaan
 {
@@ -21,34 +22,62 @@ namespace OliotListaan
 
             Random random = new Random();
 
+            List<Elain> elaimet = new List<Elain>();
+
             for (int i = 0; i < maara; i++)
             {
-                switch (i % 3)
+                if (i % 4 == 3)
                 {
-                    case 0:
-                        Koira.Koira koira = new Koira.Koira();
-                        koira.AsetaIka(random.Next(1, 15));
-                        koira.AsetaNimi(koira.Nimet[random.Next(0, koira.Nimet.Length - 1)]);
-                        koira.AsetaOnLihanSyoja(random.Next(0, 1) == 1);
-                        koira.AsetaMerinisakas(false);
-                        Console.WriteLine(koira);
-                        break;
-                    case 1:
-                        Kissa.Kissa kissa = new Kissa.Kissa();
-                        kissa.AsetaIka(random.Next(1, 15));
-                        kissa.AsetaNimi(kissa.Nimet[random.Next(0, kissa.Nimet.Length - 1)]);
-                        kissa.AsetaOnLihanSyoja(random.Next(0, 1) == 1);
-                        kissa.AsetaMerinisakas(false);
-                        Console.WriteLine(kissa);
-                        break;
-                    case 2:
-                        Papukaija.Papukaija papukaija = new Papukaija.Papukaija();
-                        papukaija.AsetaIka(random.Next(1, 15));
-                        papukaija.AsetaNimi(papukaija.Nimet[random.Next(0, papukaija.Nimet.Length - 1)]);
-                        papukaija.AsetaOnLihanSyoja(random.Next(0, 1) == 1);
-                        Console.WriteLine(papukaija);
-                        break;
+                    Hevonen.Hevonen hevonen = new Hevonen.Hevonen();
+                    hevonen.AsetaIka(random.Next(1, 15));
+                    hevonen.AsetaNimi(hevonen.Nimet[random.Next(0, hevonen.Nimet.Length - 1)]);
+                    hevonen.AsetaOnLihanSyoja(false);
+                    Console.WriteLine(hevonen.PalautaNimi());
+                    elaimet.Add(hevonen);
                 }
+                else
+                {
+                    switch (i % 3)
+                    {
+                        case 0:
+                            Koira.Koira koira = new Koira.Koira();
+                            koira.AsetaIka(random.Next(1, 15));
+                            koira.AsetaNimi(koira.Nimet[random.Next(0, koira.Nimet.Length - 1)]);
+                            koira.AsetaOnLihanSyoja(random.Next(0, 1) == 1);
+                            koira.AsetaMerinisakas(false);
+                            Console.WriteLine(koira.PalautaNimi());
+                            elaimet.Add(koira);
+                            break;
+                        case 1:
+                            Kissa.Kissa kissa = new Kissa.Kissa();
+                            kissa.AsetaIka(random.Next(1, 15));
+                            kissa.AsetaNimi(kissa.Nimet[random.Next(0, kissa.Nimet.Length - 1)]);
+                            kissa.AsetaOnLihanSyoja(random.Next(0, 1) == 1);
+                            kissa.AsetaMerinisakas(false);
+                            Console.WriteLine(kissa.PalautaNimi());
+                            elaimet.Add(kissa);
+                            break;
+                        case 2:
+                            Papukaija.Papukaija papukaija = new Papukaija.Papukaija();
+                            papukaija.AsetaIka(random.Next(1, 15));
+                            papukaija.AsetaNimi(papukaija.Nimet[random.Next(0, papukaija.Nimet.Length - 1)]);
+                            papukaija.AsetaOnLihanSyoja(random.Next(0, 1) == 1);
+                            Console.WriteLine(papukaija.PalautaNimi());
+                            elaimet.Add(papukaija);
+                            break;
+                    }
+                }
+            }
+
+            foreach (var elain in elaimet)
+            {
+                Console.WriteLine(elain);
+            }
+
+            Console.WriteLine("--- Koirat ---");
+            foreach (var olio in elaimet.OfType<Koira.Koira>())
+            {
+                Console.WriteLine(olio);
             }
         }
     }
